3cfaf8b22101a9e540920a3ccda633f20cc89543
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2023-04-21 15:08:10 -0300

    test delete

--- tests/test_delete_api.py
+++ tests/test_delete_api.py
@@ -11,13 +11,18 @@ class TestDeleteApi(unittest.TestCase):
 
     def setUp(self):
         # Configurar a URL da API e os dados do usuário
-        self.url = 'https://tolvk25ntd.execute-api.us-east-1.amazonaws.com/dev?id=09260970407'
+        self.url = 'https://tolvk25ntd.execute-api.us-east-1.amazonaws.com/dev?id={id}'
         self.headers = {'x-api-key': os.getenv('API_KEY')}
 
     def test_delete_id_invalid(self):
         response = requests.delete(self.url.replace(
-            '09', ''), headers=self.headers)
-        self.assertEqual(response.text, '"CPF 2670407 invalid!"')
+            '{id}', '3838079073'), headers=self.headers)
+        self.assertEqual(response.text, '"CPF 3838079073 invalid!"')
+
+    def test_delete_id_not_found(self):
+        response = requests.delete(self.url.replace(
+            '{id}', '38387079073'), headers=self.headers)
+        self.assertEqual(response.text, '"CPF 38387079073 is not registered!"')
 
 
 def __init__():
