f8663000768c9c98f8e01da8ee2b050c6cef832b
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2023-10-12 14:41:30 -0300

    fix

--- frontend/front-user/components/material/LinearLoader.tsx
+++ frontend/front-user/components/material/LinearLoader.tsx
@@ -4,8 +4,23 @@ import LinearProgress from "@mui/material/LinearProgress";
 
 export default function LinearLoader() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <LinearProgress />
-    </Box>
+    <div>
+      <h1>
+        <center style={{ marginTop: "20%", font: "bold" }}>
+          Carregando...
+        </center>
+      </h1>
+
+      <Box
+        sx={{
+          width: "50%",
+          marginTop: "10%",
+          marginBottom: "5%",
+          marginLeft: "25%",
+        }}
+      >
+        <LinearProgress />
+      </Box>
+    </div>
   );
 }
--- frontend/front-user/types/User.ts
+++ frontend/front-user/types/User.ts
@@ -2,5 +2,5 @@ export type User = {
   id?: string;
   userName: string;
   email: string;
-  age: number;
+  age: string;
 };
